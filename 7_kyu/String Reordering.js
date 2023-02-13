f31515c8bc0c095706775b7e342f7f4ff92ac58e
MishaKozarev <mishaaaaa1986@mail.ru>
2023-02-13 09:46:51 +0300

    feat:add String Reordering.js

---
+++ 7_kyu/String Reordering.js
@@ -0,0 +1,32 @@
+// The input will be an array of dictionaries.
+
+// Return the values as a string-seperated sentence in the order of their keys' integer equivalent (increasing order).
+
+// The keys are not reoccurring and their range is -999 < key < 999. The dictionaries' keys & values will always be strings and will always not be empty.
+
+// Example
+// Input:
+// List = [
+//         {'4': 'dog' }, {'2': 'took'}, {'3': 'his'},
+//         {'-2': 'Vatsan'}, {'5': 'for'}, {'6': 'a'}, {'12': 'spin'}
+//        ]
+
+// Output:
+// 'Vatsan took his dog for a spin'
+
+function sentence(List) {
+    let keysArr = [];
+    let allArr = [];
+    let result = [];
+    for(let i in List){
+      for(let j in List[i]){
+        allArr[j] = List[i][j]
+        keysArr.push(j);
+      }
+    }
+    keysArr.sort((a, b) => a -b)
+    for(let k = 0; k <  keysArr.length; k++){
+      result = result + allArr[keysArr[k]] + ' ';
+    }
+    return result.trim();
+  }
